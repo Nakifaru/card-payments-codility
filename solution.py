898a64b098bc8088adf9ecdaa19a0a3f2e88dfdb
Michael <michaelmwendwamumo@gmail.com>
2024-03-11 16:48:00 +0300

    Tested and working

--- solution.py
+++ solution.py
@@ -11,19 +11,24 @@ def solution(A, D):
             #display date in YYYYMMDD
             date = D[index].split('-')
             #define a variable month which is a slice of the date
-            month = date[5:-7]
+            month = date[1]
             #create a dictionary containing the frequency of each month's card payments
-            for month in no_monthly_transactions:
-                no_monthly_transactions[month] = no_monthly_transactions.get(month, 0) + 1
+            if month in no_monthly_transactions:
+                no_monthly_transactions[month] += 1
+            else:
+                no_monthly_transactions[month] = 1
+            
             #create a dictionary containing the value of card payments per month
-            for month in value_monthly_transactons:
-                value_monthly_transactons[month] = value_monthly_transactons.get(month, 0) + transaction
-
+            if month in value_monthly_transactons:
+                value_monthly_transactons[month] += transaction
+            else:
+                value_monthly_transactons[month] = transaction
+            
     #calculate no of months that will be excluded from fee
     excluded_months = 0
     #count a month as excluded if it has more than three payments with a total value less than -100, which basically is greater than 100 since payments are negative
     for key,value in no_monthly_transactions.items():
-        if value > 3 and value_monthly_transactons[key] < -100:
+        if value >= 3 and value_monthly_transactons[key] <= -100:
             excluded_months += 1
     
     #assign a 5 to a variable for easy maintenance incase monthly fee changes
@@ -38,5 +43,6 @@ def solution(A, D):
     print (final_balance)
     return final_balance
 
-solution([100, 100, 100, -10], ["2020-01-01", "2020-12-22", "2020-12-03", "2020-12-29"])
+# solution([100, 100, 100, -10], ["2020-01-01", "2020-12-22", "2020-12-03", "2020-12-29"])
+solution([180, -50, -25, -25], ["2020-01-01", "2020-01-01", "2020-01-01", "2020-01-31"])
     
